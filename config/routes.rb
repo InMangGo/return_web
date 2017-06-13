3339c1f18420a7f8db651e1bfe82c9e0d516ef0f
Ricky <1308543433@qq.com>
2017-06-13 22:27:54 +0800

    添加项目页面分布

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+
   namespace :backend do
     root 'dashboard#index'
   end
+
+  namespace :web, path: '' do
+    get :case, to: 'home#case'
+    get :case_video, to: 'home#case_video'
+    get :case_project, to: 'home#case_project'
+    get :about, to: 'home#about'
+    get :contact, to: 'home#contact'
+  end
+
+  root 'web/home#index'
 end
