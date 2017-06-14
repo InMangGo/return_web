6e6530654d0ee151868cfcd1b8c6fc386d074c23
XingKaiLing <963255230@qq.com>
2017-06-14 10:59:59 +0800

    基础配置

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def html_body_classes
+    controller_path_action_name = "#{controller.controller_path.gsub('/', '__')}__#{controller.action_name}"
+    if @body_class.present?
+      "#{@body_class} #{controller_path_action_name}"
+    else
+      controller_path_action_name.to_s
+    end
+  end
 end
